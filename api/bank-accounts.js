7ba59be0eb25e46ef65ff1539636567ae9569543
MarxMad <gerardo.gerry.pedrizco@gmail.com>
2025-07-09 09:25:56 -0600

    modulos back

--- api/bank-accounts.js
+++ api/bank-accounts.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { buildJunoAuthHeader } from './_utils';
+import { buildJunoAuthHeader } from './_utils.js';
 
 export default async function handler(req, res) {
   if (req.method !== 'GET') {
--- api/redeem.js
+++ api/redeem.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { buildJunoAuthHeader } from './_utils';
+import { buildJunoAuthHeader } from './_utils.js';
 import { v4 as uuidv4 } from 'uuid';
 
 export default async function handler(req, res) {
