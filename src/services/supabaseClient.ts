63f4a93095710ce989793cfe9fa8263daace8199
MarxMad <gerardo.gerry.pedrizco@gmail.com>
2025-07-12 00:35:31 -0600

    supabase2

--- src/services/supabaseClient.ts
+++ src/services/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://xspmzoyuhcvwbjqrwxov.supabase.co'; // Reemplaza por tu URL si cambia
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhzcG16b3l1aGN2d2Jqcnd4b3YiLCJyb2xlIjoiYW5vbiIsImlhdCI6MTcxNzA2NjQwNSwiZXhwIjo0ODczNjQyMDA1fQ.2Qw1QwQnQnQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQw'; // Reemplaza por tu API Key o usa variable de entorno
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey); 
